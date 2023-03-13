a521b80883ce73b0a5d2a5e0dcf3ea4a9f348fc0
Myo Thet Kyaw <73188846+FrankyKyaw@users.noreply.github.com>
2023-03-12 23:00:10 -0400

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import Chat from "./Chat";
-import Home from "./pages/Home";
-import { io } from "socket.io-client";
+import io from "socket.io-client";
 import { useState } from "react";
+import Chat from "./Chat";
 
 const socket = io.connect("http://localhost:3001");
 
@@ -22,89 +18,163 @@ function App() {
   };
 
   return (
-    <div className="flex flex-col pt-32 w-full items-center h-screen bg-gray-800">
+    <div className="App">
       {!showChat ? (
-        <div className="block rounded-lg bg-white p-10">
-          <h3 className="flex justify-center">Join a chat</h3>
-          <div className="flex space-x-3 p-4">
-            <input
-              className="border p-3"
-              type="text"
-              placeholder="Name..."
-              onChange={(event) => {
-                setUsername(event.target.value);
-              }}
-            />
-            <input
-              className="border p-3"
-              type="text"
-              placeholder="Room ID..."
-              onChange={(event) => {
-                setRoom(event.target.value);
-              }}
-            />
-            <button
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-              onClick={joinRoom}
-            >
-              Join a Room
-            </button>
-          </div>
+        <div className="joinChatContainer">
+          <h3>Join A Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
         </div>
       ) : (
         <Chat socket={socket} username={username} room={room} />
       )}
     </div>
-
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route path="/register" element={<Register />} />
-    //     <Route path="/login" element={<Login />} />
-    //     <Route path="/chat" element={<Chat />} />
-    //     <Route path='/' element={<Home/>}/>
-    //   </Routes>
-    // </BrowserRouter>
-    // <div className="App">
-    //   <h1 className="text-3xl font-bold underline">
-    //   <button></button>
-    // </h1>
-    // </div>
   );
 }
 
 export default App;
 
-// const [room, setRoom] = useState('');
-//   const [message, setMessage] = useState("");
-//   const [messageReceived, setMessageReceived] = useState("");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import "./App.css";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Register from "./pages/Register";
+// import Login from "./pages/Login";
+// import Chat from "./Chat";
+// import Home from "./pages/Home";
+// import { io } from "socket.io-client";
+// import { useState } from "react";
+
+// const socket = io.connect("http://localhost:3001");
+
+// function App() {
+//   const [username, setUsername] = useState("");
+//   const [room, setRoom] = useState("");
+//   const [showChat, setShowChat] = useState(false);
 
 //   const joinRoom = () => {
-//     if (room !== "") {
+//     if (username !== "" && room !== "") {
 //       socket.emit("join_room", room);
+//       setShowChat(true);
 //     }
-//   }
-//   const sendMessage = () => {
-//     socket.emit("send_message", { message, room });
 //   };
 
-//   useEffect(() => {
-//     socket.on("receive_message", (data) => {
-//       setMessageReceived(data.message);
-//     });
-//   }, [socket]);
-// <input
-// onChange={(event) => {
-//   setRoom(event.target.value);
-// }}
-// placeholder="Room Number..."
-// />
-// <button onClick={joinRoom}>Join Room</button>
-// <input
-// onChange={(event) => {
-//   setMessage(event.target.value);
-// }}
-// placeholder="Message..."
-// />
-// <button onClick={sendMessage}>Send Message</button>
-// <h1>Message: </h1>
-// {messageReceived}
+//   return (
+//     <div className="flex flex-col pt-32 w-full items-center h-screen bg-gray-800">
+//       {!showChat ? (
+//         <div className="block rounded-lg bg-white p-10">
+//           <h3 className="flex justify-center">Join a chat</h3>
+//           <div className="flex space-x-3 p-4">
+//             <input
+//               className="border p-3"
+//               type="text"
+//               placeholder="Name..."
+//               onChange={(event) => {
+//                 setUsername(event.target.value);
+//               }}
+//             />
+//             <input
+//               className="border p-3"
+//               type="text"
+//               placeholder="Room ID..."
+//               onChange={(event) => {
+//                 setRoom(event.target.value);
+//               }}
+//             />
+//             <button
+//               className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+//               onClick={joinRoom}
+//             >
+//               Join a Room
+//             </button>
+//           </div>
+//         </div>
+//       ) : (
+//         <Chat socket={socket} username={username} room={room} />
+//       )}
+//     </div>
+
+//     // <BrowserRouter>
+//     //   <Routes>
+//     //     <Route path="/register" element={<Register />} />
+//     //     <Route path="/login" element={<Login />} />
+//     //     <Route path="/chat" element={<Chat />} />
+//     //     <Route path='/' element={<Home/>}/>
+//     //   </Routes>
+//     // </BrowserRouter>
+//     // <div className="App">
+//     //   <h1 className="text-3xl font-bold underline">
+//     //   <button></button>
+//     // </h1>
+//     // </div>
+//   );
+// }
+
+// export default App;
+
+// // const [room, setRoom] = useState('');
+// //   const [message, setMessage] = useState("");
+// //   const [messageReceived, setMessageReceived] = useState("");
+
+// //   const joinRoom = () => {
+// //     if (room !== "") {
+// //       socket.emit("join_room", room);
+// //     }
+// //   }
+// //   const sendMessage = () => {
+// //     socket.emit("send_message", { message, room });
+// //   };
+
+// //   useEffect(() => {
+// //     socket.on("receive_message", (data) => {
+// //       setMessageReceived(data.message);
+// //     });
+// //   }, [socket]);
+// // <input
+// // onChange={(event) => {
+// //   setRoom(event.target.value);
+// // }}
+// // placeholder="Room Number..."
+// // />
+// // <button onClick={joinRoom}>Join Room</button>
+// // <input
+// // onChange={(event) => {
+// //   setMessage(event.target.value);
+// // }}
+// // placeholder="Message..."
+// // />
+// // <button onClick={sendMessage}>Send Message</button>
+// // <h1>Message: </h1>
+// // {messageReceived}
--- client/src/Chat.js
+++ client/src/Chat.js
@@ -1,7 +1,6 @@
-import React from "react";
-import { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Chat({ socket, username, room }) {
+function Chat({ socket, username, room }) {
   const [currentMessage, setCurrentMessage] = useState("");
   const [messageList, setMessageList] = useState([]);
 
@@ -14,45 +13,41 @@ export default function Chat({ socket, username, room }) {
         time:
           new Date(Date.now()).getHours() +
           ":" +
-          new Date(Date.now()).getMinutes() +
-          ":" + 
-          new Date(Date.now()).getSeconds(),
+          new Date(Date.now()).getMinutes(),
       };
 
       await socket.emit("send_message", messageData);
-      
-      setMessageList(prevState => [...prevState, messageData]);
-      console.log(messageList)
+      setMessageList((list) => [...list, messageData]);
+      setCurrentMessage("");
     }
   };
 
   useEffect(() => {
-    
-    socket.on("receive_message", (data) => {
+    socket.once("receive_message", (data) => {
       setMessageList((list) => [...list, data]);
-      console.log(messageList)
+      console.log("receive_message triggered:", data);
+      return;
     });
   }, [socket]);
 
   return (
-    <div className="bg-gray-700 text-white p-4 rounded-md">
-      <div>Live Chat</div>
-      <div className="h-60">
+    <div className="chat-window">
+      <div className="chat-header">
+        <p>Live Chat</p>
+      </div>
+      <div className="chat-body">
         {messageList.map((messageContent, index) => {
           return (
             <div
               key={index}
-              className={`flex w-auto max-w-xs px-4 py-1 rounded-lg ${
-                username === messageContent.author
-                  ? "bg-blue-400 justify-end text-white"
-                  : "bg-gray-200 text-gray-800"
-              }`}
+              className="message"
+              id={username === messageContent.author ? "you" : "other"}
             >
               <div>
-                <div className="inline-block max-w-xs bg-blue-400 px-4 py-1 rounded-lg text-white">
+                <div className="message-content">
                   <p>{messageContent.message}</p>
                 </div>
-                <div>
+                <div className="message-meta">
                   <p id="time">{messageContent.time}</p>
                   <p id="author">{messageContent.author}</p>
                 </div>
@@ -61,32 +56,118 @@ export default function Chat({ socket, username, room }) {
           );
         })}
       </div>
-      <div>
+      <div className="chat-footer">
         <input
           type="text"
           value={currentMessage}
+          placeholder="Hey..."
           onChange={(event) => {
             setCurrentMessage(event.target.value);
           }}
-          placeholder="Write your message!"
-          class="focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-6 bg-gray-200 py-3"
+          onKeyPress={(event) => {
+            event.key === "Enter" && sendMessage();
+          }}
         />
-        <button
-          type="button"
-          onClick={sendMessage}
-          className="inline-flex items-center justify-center  px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
-        >
-          <span class="font-bold">Send</span>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            class="h-6 w-6 ml-2 transform rotate-90"
-          >
-            <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-          </svg>
-        </button>
+        <button onClick={sendMessage}>&#9658;</button>
       </div>
     </div>
   );
 }
+
+export default Chat;
+
+// import React from "react";
+// import { useState, useEffect } from "react";
+
+// export default function Chat({ socket, username, room }) {
+//   const [currentMessage, setCurrentMessage] = useState("");
+//   const [messageList, setMessageList] = useState([]);
+
+//   const sendMessage = async () => {
+//     if (currentMessage !== "") {
+//       const messageData = {
+//         room: room,
+//         author: username,
+//         message: currentMessage,
+//         time:
+//           new Date(Date.now()).getHours() +
+//           ":" +
+//           new Date(Date.now()).getMinutes() +
+//           ":" +
+//           new Date(Date.now()).getSeconds(),
+//       };
+
+//       await socket.emit("send_message", messageData);
+
+//       setMessageList(prevState => [...prevState, messageData]);
+//       setCurrentMessage("");
+//     }
+//   };
+
+//   useEffect(() => {
+
+//     socket.on("receive_message", (data) => {
+//       setMessageList((list) => [...list, data]);
+//       console.log(messageList)
+//     });
+//   }, []);
+
+//   return (
+//     <div className="bg-gray-700 text-white p-4 rounded-md">
+//       <div>Live Chat</div>
+//       <div className="h-60">
+//         {messageList.map((messageContent, index) => {
+//           return (
+//             <div
+//               key={index}
+//               className={`flex w-auto max-w-xs px-4 py-1 rounded-lg ${
+//                 username === messageContent.author
+//                   ? "bg-blue-400 justify-end text-white"
+//                   : "bg-gray-200 text-gray-800"
+//               }`}
+//             >
+//               <div>
+//                 <div className="inline-block max-w-xs bg-blue-400 px-4 py-1 rounded-lg text-white">
+//                   <p>{messageContent.message}</p>
+//                 </div>
+//                 <div>
+//                   <p id="time">{messageContent.time}</p>
+//                   <p id="author">{messageContent.author}</p>
+//                 </div>
+//               </div>
+//             </div>
+//           );
+//         })}
+//       </div>
+//       <div>
+//         <input
+//           type="text"
+//           value={currentMessage}
+//           onChange={(event) => {
+//             setCurrentMessage(event.target.value);
+//           }}
+//           placeholder="Write your message!"
+//           className="focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-6 bg-gray-200 py-3"
+//           onKeyPress={(event) => {
+//             event.key === "Enter" && sendMessage();
+//           }}
+//         />
+//         <button
+//           type="button"
+//           onClick={sendMessage}
+//           className="inline-flex items-center justify-center  px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none"
+//         >
+//           <span class="font-bold">Send</span>
+//           <svg
+//             xmlns="http://www.w3.org/2000/svg"
+//             viewBox="0 0 20 20"
+//             fill="currentColor"
+//             className="h-6 w-6 ml-2 transform rotate-90"
+//           >
+//             <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+//           </svg>
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
