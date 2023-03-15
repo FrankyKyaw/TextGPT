37f44e6278667e36bb491eeba20ff74d812e0837
Myo Thet Kyaw <73188846+FrankyKyaw@users.noreply.github.com>
2023-03-15 17:53:37 -0400

    adding openai library and adding api

--- server/index.js
+++ server/index.js
@@ -1,8 +1,18 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
 app.use(cors());
 
 const server = http.createServer(app);
@@ -29,6 +39,31 @@ io.on("connection", (socket) => {
   socket.on("disconnect", () => {
     console.log("User Disconnected", socket.id);
   });
+});
+
+app.post("/suggest", async (req, res) => {
+  try {
+    const message = req.body.message;
+
+    const completions = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are a helpful conversational assistant that suggests texts based on incoming messages.",
+        },
+        {
+          role: "user",
+          content: `Suggest a message to text back based on this text: ${message}`,
+        },
+      ],
+    });
+    res.json({ completions });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send("An error occured while processing the message.");
+  }
 });
 
 server.listen(3001, () => {
